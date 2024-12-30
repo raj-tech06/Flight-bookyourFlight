886710c1b88e565d2b393672ee5a8adc19f9dd40
Abhishek Sahu <rjdeveloper07@gmail.com>
2024-12-30 22:29:04 +0530

    30th night

--- flight.js
+++ flight.js
@@ -30,21 +30,45 @@ sec.innerHTML = (currentTime.getSeconds()<10?"0":"")+currentTime.getSeconds();
 },1000)
 
 
-
-
-function showDropdown() {
-    let dropdown = document.querySelector("#suggdropdown");
+// --------------------------------inputsropdown-----------------------
+//drop-from
+function showDropdown1() {
+    let dropdown = document.querySelector("#suggdropdown1");
     dropdown.style.display = "block"; 
 }
 
 
-function selectItem(item) {
-    document.querySelector("#input00").value = item;
-    document.querySelector("#suggdropdown").style.display = "none"; 
+function selectItem1(item) {
+    document.querySelector("#input1").value = item;
+    document.querySelector("#suggdropdown1").style.display = "none"; 
 }
+//drop-to
+function showDropdown2() {
+    let dropdown = document.querySelector("#suggdropdown2");
+    dropdown.style.display = "block"; 
+}
+
+
+function selectItem2(item) {
+    document.querySelector("#input2").value = item;
+    document.querySelector("#suggdropdown2").style.display = "none"; 
+}
+//drop-travel
+function showDropdown3() {
+    let dropdown = document.querySelector("#suggdropdown3");
+    dropdown.style.display = "block"; 
+}
+
+
+function selectItem3(item) {
+    document.querySelector("#input3").value = item;
+    document.querySelector("#suggdropdown3").style.display = "none"; 
+}
+// ----------------------------------------------------------------------------------------
 
 let hide=()=>{
     let input = document.querySelector("#input00")
 let dropdown = document.querySelector("#suggdropdown");
 input.innerHTML=dropdown.style.display="none";
 }
+// ----------------------end drop-----------------------
